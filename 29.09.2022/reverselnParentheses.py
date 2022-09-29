4247f8e0372422e9c6c7d8c5886254ed8b0fccd2
avagova <86437591+avagova@users.noreply.github.com>
2022-09-29 10:29:55 +0400

    Update reverselnParentheses.py

--- 29.09.2022/reverselnParentheses.py
+++ 29.09.2022/reverselnParentheses.py
@@ -1,1 +1,10 @@
-
+def solution(arr):
+    ind = []
+    res = ""
+    for j in range(len(arr)):
+        if arr[j] == '(':
+            ind.insert(0,j)
+        elif arr[j] == ')':
+            arr = arr[:ind[0]+1] + arr[ind[0]+1:j][::-1] + arr[j:]
+            del ind[0]
+    return arr.replace('(','').replace(')','')
